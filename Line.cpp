d97d5b5bd9f08c89ec8335520900b624a509d79a
shariqm05 <shariqm2005@gmail.com>
2024-10-22 21:51:14 -0400

    AddStop() GetLineName(), PrintLineDetails() Line Class Definitions Fully implemeneted and defined the follwing functions in Line.cpp
    AddStop(): add a Stop add the end of a line using dynamic memory allocation
    
    GetLineName(): Returns the Line GetLineName
    
    PrintLineDetails(): Prints all the stops numbers and names in the line

--- Line.cpp
+++ Line.cpp
@@ -26,6 +26,67 @@ Line::~Line(){
     while(curr != nullptr){ //Iterate until the end of the line
         Stop* next = curr->GetNext();
         delete curr;
-        curr = next
+        curr = next;
+    }
+    //free m_head and m_tail ptrs
+    m_head = nullptr;  
+    m_tail = nullptr;
+}
+
+//FUNCTIONS
+
+// Name: GetLineName
+  // Desc: Returns the name of the line
+  // Preconditions: Line must have a name
+  // Postconditions: Returns m_lineName;
+string Line::GetLineName(){return m_lineName;}
+
+  // Name: AddStop()
+  // Desc: Dynamically allocates new stop and inserts at end of line
+  //        (passes stop number and name)
+  // Preconditions: Line allocated and data for stop available
+  // Postconditions: New stop inserted in end of line
+void Line::AddStop(int num, string name){
+
+    Stop* temp = new Stop(num, name); //make new stop/node
+
+    if (m_head == nullptr){ //if the line is empty
+        m_head = temp; //update m_head to point at temp's node (since its the only Stop in the line)
+        temp->SetNext(nullptr); //set temp's m_next to nullptr.
+        m_tail = temp; //update tail to point at temp's node
+    }
+    else{ //If the line is not empty
+        m_tail->SetNext(temp); // set last node to point at temp
+        temp->SetNext(nullptr); //set newly added Stop at the end of the line to nullptr
+        m_tail = temp; //update m_tail to represent new end of the line
+        temp = nullptr; //free the temp ptr
+    }
+    temp = nullptr; //free the temp ptr
+  }
+
+  // Name: PrintLineDetails()
+  // Desc: Prints information about a specific line
+  // Preconditions: Line allocated and data for next stop available
+  // Postconditions: Displays list of each stop on a line
+  //                 including number, name, and (lineName)
+void Line::PrintLineDetails(){
+    cout << "**" << m_lineName << " Line Stops**" << endl;
+    Stop* curr = m_head;
+
+    while(curr != nullptr){
+        cout << curr->GetNumber() << ". " << curr->GetName()<< "(" << m_lineName << ")" << endl;
+        curr = curr->GetNext(); //increment curr by a node
     }
 }
+
+// Name: CalculateDistance(string start, string end)
+  // Desc: Calculates the number of stops between the
+  //        starting station and ending station
+  // Preconditions: Line allocated with start and stop on correct line
+  // Postconditions: Returns number of stops between two stations
+  // There will always be at least two stops (start != stop)
+  // For example, if the line is station 1 to station 2, the distance is 2
+  // If the line is station 1 to station 4, the distance is 4
+  int Line::CalculateDistance(string start, string end){
+    
+  }
